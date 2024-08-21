0f8af0154821e269a40213e597ad454e51b2b25f
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2024-08-21 13:14:04 -0700

    trying more stuff

--- src/ephys_alignment_gui/load_data_local.py
+++ src/ephys_alignment_gui/load_data_local.py
@@ -207,14 +207,13 @@ class LoadDataLocal:
         with open(xyz_file[0], "r") as f:
             user_picks = json.load(f)
     
-        xyz_picks = np.array(user_picks["xyz_picks"]) * 1000
+        xyz_picks = np.array(user_picks["xyz_picks"])
 
         # This is a hack and will be fixed in the future!
-        
-        xyz_picks[:,1] = 406-xyz_picks[:,1]-1500
-        xyz_picks[:,2]= xyz_picks[:,2]-1500
-        xyz_picks[:,0] = xyz_picks[:,0]-1500
-
+        xyz_picks[:,1] = xyz_picks[:, 1] - 1.5
+        xyz_picks[:,2]= xyz_picks[:,2] + 1.5
+        xyz_picks[:,0] = xyz_picks[:, 0] - 1.5 - 4.06
+        xyz_picks = xyz_picks * 100
         print(xyz_picks)
         return xyz_picks
 
