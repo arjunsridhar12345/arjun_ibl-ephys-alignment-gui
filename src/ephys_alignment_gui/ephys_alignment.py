3918d2ce7f530e435f80bec51e2dd32da664bd32
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2024-08-22 12:41:41 -0700

    change mode to clip

--- src/ephys_alignment_gui/ephys_alignment.py
+++ src/ephys_alignment_gui/ephys_alignment.py
@@ -285,7 +285,7 @@ class EphysAlignment:
         if not brain_atlas:
             brain_atlas = atlas.AllenAtlas(25)
 
-        region_ids = brain_atlas.get_labels(xyz_coords, mapping=mapping)
+        region_ids = brain_atlas.get_labels(xyz_coords, mapping=mapping, mode='clip')
         region_info = brain_atlas.regions.get(region_ids)
         boundaries = np.where(np.diff(region_info.id))[0]
         region = np.empty((boundaries.size + 1, 2))
