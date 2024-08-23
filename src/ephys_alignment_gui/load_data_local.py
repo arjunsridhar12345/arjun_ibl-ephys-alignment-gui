227b4a8c35b949bc673f0df4053a0d18bbb9130c
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2024-08-23 14:46:45 -0700

    index into spacing array

--- src/ephys_alignment_gui/load_data_local.py
+++ src/ephys_alignment_gui/load_data_local.py
@@ -210,9 +210,9 @@ class LoadDataLocal:
         xyz_picks = np.array(user_picks["xyz_picks"])
         # This is a hack and will be fixed in the future!
 
-        xyz_picks[:,1] = xyz_picks[:, 1] * self.brain_atlas.spacing - 1.5
-        xyz_picks[:,2] = xyz_picks[:, 2] * self.brain_atlas.spacing + 1.5
-        xyz_picks[:,0] = xyz_picks[:, 0] * self.brain_atlas.spacing - 1.5
+        xyz_picks[:,1] = xyz_picks[:, 1] * self.brain_atlas.spacing[1] - 1.5
+        xyz_picks[:,2] = xyz_picks[:, 2] * self.brain_atlas.spacing[2] + 1.5
+        xyz_picks[:,0] = xyz_picks[:, 0] * self.brain_atlas.spacing[0] - 1.5
 
         print(xyz_picks)
         return xyz_picks
