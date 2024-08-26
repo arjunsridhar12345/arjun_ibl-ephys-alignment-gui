a8977afd269adb2f2f41d9fee22b921b05763f9e
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2024-08-26 10:34:48 -0700

    try to get region ids from volume

--- src/ephys_alignment_gui/load_data_local.py
+++ src/ephys_alignment_gui/load_data_local.py
@@ -210,10 +210,9 @@ class LoadDataLocal:
         xyz_picks = np.array(user_picks["xyz_picks"])
         # This is a hack and will be fixed in the future!
 
-        xyz_picks[:,1] = xyz_picks[:, 1] * (self.brain_atlas.spacing[1] * 1000)
-        xyz_picks[:,2] = xyz_picks[:, 2] * (self.brain_atlas.spacing[2] * 1000)
-        xyz_picks[:,0] = xyz_picks[:, 0] * (self.brain_atlas.spacing[0] * 1000)
-
+        xyz_picks[:,1] = xyz_picks[:, 1] * self.brain_atlas.spacing[1]
+        xyz_picks[:,2] = xyz_picks[:, 2] * self.brain_atlas.spacing[2]
+        xyz_picks[:,0] = xyz_picks[:, 0] * self.brain_atlas.spacing[0]
         print(xyz_picks)
         return xyz_picks
 
