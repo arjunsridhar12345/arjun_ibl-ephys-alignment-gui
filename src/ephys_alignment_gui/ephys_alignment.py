6f63bb32ffc6f361f76ce9b92470c80b2aa21713
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2024-08-26 16:13:53 -0700

    try using get lregion id labels function

--- src/ephys_alignment_gui/ephys_alignment.py
+++ src/ephys_alignment_gui/ephys_alignment.py
@@ -289,11 +289,11 @@ class EphysAlignment:
         if not brain_atlas:
             brain_atlas = atlas.AllenAtlas(25)
 
-        #region_ids = brain_atlas.get_labels(xyz_coords, mapping=mapping, mode='clip')
+        region_ids = brain_atlas.get_labels(xyz_coords, mapping=mapping, mode='clip')
         xyz_indices = np.round(xyz_coords).astype(np.uint16)
         xyz_indices = xyz_indices[(xyz_indices[:, 0] < brain_atlas.image.shape[1]) & (xyz_indices[:, 1] < brain_atlas.image.shape[0])
                                                                                       & (xyz_indices[:, 2] < brain_atlas.image.shape[2])]
-        region_ids = brain_atlas.image[xyz_indices[:, 1], xyz_indices[:, 0], xyz_indices[:, 2]]
+      
         print("Region ids", region_ids)
 
         region_info = brain_atlas.regions.get(region_ids)
