61144b6fa3fe2a632410f8d749f09067f6621915
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2024-08-26 15:07:44 -0700

    remove check for bounds for now

--- src/ephys_alignment_gui/ephys_alignment.py
+++ src/ephys_alignment_gui/ephys_alignment.py
@@ -41,14 +41,14 @@ class EphysAlignment:
                                                              self.sampling_trk -
                                                              self.sampling_trk[0])
         # ensure none of the track is outside the y or x lim of atlas
-        
+        """
         xlim = np.bitwise_and(self.xyz_samples[:, 0] > self.brain_atlas.bc.xlim[0],
                               self.xyz_samples[:, 0] < self.brain_atlas.bc.xlim[1])
         ylim = np.bitwise_and(self.xyz_samples[:, 1] < self.brain_atlas.bc.ylim[0],
                               self.xyz_samples[:, 1] > self.brain_atlas.bc.ylim[1])
         rem = np.bitwise_and(xlim, ylim)
         self.xyz_samples = self.xyz_samples[rem]
-        
+        """
         self.region, self.region_label, self.region_colour, self.region_id\
             = self.get_histology_regions(self.xyz_samples, self.sampling_trk, self.brain_atlas)
 
@@ -289,7 +289,6 @@ class EphysAlignment:
         if not brain_atlas:
             brain_atlas = atlas.AllenAtlas(25)
 
-        print('Coords', xyz_coords)
         region_ids = brain_atlas.get_labels(xyz_coords, mapping=mapping, mode='clip')
       
         region_info = brain_atlas.regions.get(region_ids)
