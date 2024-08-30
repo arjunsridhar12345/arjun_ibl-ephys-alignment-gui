e4c509ba2b08a96108a6ff0bc66456364da65a8c
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2024-08-30 12:24:39 -0700

    try to fix orientation spacing and offset

--- src/ephys_alignment_gui/custom_atlas.py
+++ src/ephys_alignment_gui/custom_atlas.py
@@ -268,7 +268,7 @@ class CustomAtlas(BrainAtlas):
         # Convert sitk to the (ap, ml, dv) np array needed by BrainAtlas
         IMG2 = sitk.DICOMOrient(IMG,self.read_string) 
         self.image = sitk.GetArrayFromImage(IMG2)
-        self.offset = IMG.GetOrigin()
+        self.offset = IMG2.GetOrigin()
         self.spacing = IMG2.GetSpacing()[0] * 1000
         return IMG2.GetSpacing()
         
