008316b0f046ac3c979f9edde156cc53fce98b69
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2024-09-04 15:26:28 -0700

    use xyz track to interpolate

--- src/ephys_alignment_gui/ephys_alignment.py
+++ src/ephys_alignment_gui/ephys_alignment.py
@@ -524,7 +524,7 @@ class EphysAlignment:
         if depths is None:
             depths = self.chn_depths
         # nb using scipy here so we can change to cubic spline if needed
-        channel_depths_track = self.feature2track(depths, feature, track) - self.track_extent[0]
+        channel_depths_track = self.feature2track(depths, feature, track) - self.xyz_track[0]
         xyz_channels = histology.interpolate_along_track(self.xyz_track, channel_depths_track)
         return xyz_channels
 
