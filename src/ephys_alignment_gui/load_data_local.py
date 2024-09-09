3496519ad299783305746d952ef12d7adfbbc5b3
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2024-09-09 15:48:15 -0700

    apply correct axis offsets

--- src/ephys_alignment_gui/load_data_local.py
+++ src/ephys_alignment_gui/load_data_local.py
@@ -209,8 +209,8 @@ class LoadDataLocal:
         xyz_picks = np.array(user_picks["xyz_picks"])
 
         # This is a hack and will be fixed in the future!
-        xyz_picks[:, 0] = -(xyz_picks[:, 0] - self.brain_atlas.offset[0])
-        xyz_picks[:, 1] = xyz_picks[:, 1] - self.brain_atlas.offset[1]
+        xyz_picks[:, 0] = -(xyz_picks[:, 0] - self.brain_atlas.offset[1])
+        xyz_picks[:, 1] = xyz_picks[:, 1] - self.brain_atlas.offset[0]
         xyz_picks[:, 2] = -(xyz_picks[:, 2] - self.brain_atlas.offset[2])
         xyz_picks = (xyz_picks * 1000) / self.brain_atlas.spacing
         
