3ebbc5a620cd5e0c9fcc4d4e8a87a999cdf68303
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2024-09-09 15:14:28 -0700

    flip axis

--- src/ephys_alignment_gui/load_data_local.py
+++ src/ephys_alignment_gui/load_data_local.py
@@ -226,7 +226,7 @@ class LoadDataLocal:
         ]
         """
         ccf_slice = self.brain_atlas.image[xyz_indices[:, 1], :, xyz_indices[:, 2]]
-        #ccf_slice = np.swapaxes(ccf_slice, 0, 1)
+        ccf_slice = np.flipud(np.swapaxes(ccf_slice, 0, 1))
         print('Shape', ccf_slice.shape)
         label_slice = self.brain_atlas._label2rgb(
             self.brain_atlas.label[xyz_indices[:, 1], :, xyz_indices[:, 2]]
