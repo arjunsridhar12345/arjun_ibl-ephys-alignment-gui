21355921e91e58c577706c9b4c7e221ed92a5a36
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2024-09-10 09:45:00 -0700

    remove track debug print

--- src/ephys_alignment_gui/ephys_alignment.py
+++ src/ephys_alignment_gui/ephys_alignment.py
@@ -158,7 +158,6 @@ class EphysAlignment:
                               & (xyz_track[:, 2] < self.brain_atlas.image.shape[2])]
 
         xyz_track = xyz_track[(xyz_track[:, 2] > 0)] 
-        print("track", xyz_track)
         
         tip_distance = _cumulative_distance(xyz_track)[1] + (TIP_SIZE_UM / self.brain_atlas.spacing)
         track_length = _cumulative_distance(xyz_track)[-1]
