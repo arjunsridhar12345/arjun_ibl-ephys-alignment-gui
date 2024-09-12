6cc0d4e9c967c87996ee27c6c609fb33d8c339ec
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2024-09-12 13:47:27 -0700

    remove million scaling from main gui

--- src/ephys_alignment_gui/launch_gui.py
+++ src/ephys_alignment_gui/launch_gui.py
@@ -616,11 +616,11 @@ class MainWindow(QtWidgets.QMainWindow, ephys_gui.Setup):
         # lines can't be moved outside interpolation bounds
         # Add offset of 1um to keep within bounds of interpolation
         offset = 1
-        self.tip_pos.setBounds((self.track[self.idx][0] * 1e6 + offset,
-                                self.track[self.idx][-1] * 1e6 -
+        self.tip_pos.setBounds((self.track[self.idx][0]  + offset,
+                                self.track[self.idx][-1]  -
                                 (self.probe_top + offset)))
-        self.top_pos.setBounds((self.track[self.idx][0] * 1e6 + (self.probe_top + offset),
-                                self.track[self.idx][-1] * 1e6 - offset))
+        self.top_pos.setBounds((self.track[self.idx][0]  + (self.probe_top + offset),
+                                self.track[self.idx][-1]  - offset))
         self.tip_pos.sigPositionChanged.connect(self.tip_line_moved)
         self.top_pos.sigPositionChanged.connect(self.top_line_moved)
 
@@ -707,7 +707,7 @@ class MainWindow(QtWidgets.QMainWindow, ephys_gui.Setup):
                                            self.hist_nearby_col)):
             colour = QtGui.QColor(c)
             plot = pg.PlotCurveItem()
-            plot.setData(x=x, y=y * 1e6, fillLevel=10, fillOutline=True)
+            plot.setData(x=x, y=y , fillLevel=10, fillOutline=True)
             plot.setBrush(colour)
             plot.setPen(colour)
             fig.addItem(plot)
@@ -717,7 +717,7 @@ class MainWindow(QtWidgets.QMainWindow, ephys_gui.Setup):
             colour = QtGui.QColor(c)
             colour.setAlpha(70)
             plot = pg.PlotCurveItem()
-            plot.setData(x=x, y=y * 1e6, fillLevel=10, fillOutline=True)
+            plot.setData(x=x, y=y , fillLevel=10, fillOutline=True)
             plot.setBrush(colour)
             plot.setPen(colour)
             fig.addItem(plot)
@@ -740,7 +740,7 @@ class MainWindow(QtWidgets.QMainWindow, ephys_gui.Setup):
         if not self.histology_exists:
             return
 
-        self.track[self.idx] = (self.track[self.idx_prev] + self.tip_pos.value() / 1e6)
+        self.track[self.idx] = (self.track[self.idx_prev] + self.tip_pos.value() )
         self.features[self.idx] = (self.features[self.idx_prev])
 
         self.get_scaled_histology()
@@ -755,9 +755,9 @@ class MainWindow(QtWidgets.QMainWindow, ephys_gui.Setup):
             return
 
         # Track --> histology plot
-        line_track = np.array([line[0].pos().y() for line in self.lines_tracks]) / 1e6
+        line_track = np.array([line[0].pos().y() for line in self.lines_tracks]) 
         # Feature --> ephys data plots
-        line_feature = np.array([line[0].pos().y() for line in self.lines_features]) / 1e6
+        line_feature = np.array([line[0].pos().y() for line in self.lines_features]) 
         depths_track = np.sort(np.r_[self.track[self.idx_prev][[0, -1]], line_track])
 
         self.track[self.idx] = self.ephysalign.feature2track(depths_track,
@@ -854,15 +854,15 @@ class MainWindow(QtWidgets.QMainWindow, ephys_gui.Setup):
         if not self.histology_exists:
             return
 
-        self.fit_plot.setData(x=self.features[self.idx] * 1e6,
-                              y=self.track[self.idx] * 1e6)
-        self.fit_scatter.setData(x=self.features[self.idx] * 1e6,
-                                 y=self.track[self.idx] * 1e6)
+        self.fit_plot.setData(x=self.features[self.idx] ,
+                              y=self.track[self.idx] )
+        self.fit_scatter.setData(x=self.features[self.idx] ,
+                                 y=self.track[self.idx] )
 
-        depth_lin = self.ephysalign.feature2track_lin(self.depth / 1e6, self.features[self.idx],
+        depth_lin = self.ephysalign.feature2track_lin(self.depth , self.features[self.idx],
                                                       self.track[self.idx])
         if np.any(depth_lin):
-            self.fit_plot_lin.setData(x=self.depth, y=depth_lin * 1e6)
+            self.fit_plot_lin.setData(x=self.depth, y=depth_lin )
         else:
             self.fit_plot_lin.setData()
 
@@ -1336,7 +1336,7 @@ class MainWindow(QtWidgets.QMainWindow, ephys_gui.Setup):
         self.toggle_labels_button_pressed()
         self.plot_scale_factor()
         if np.any(self.feature_prev):
-            self.create_lines(self.feature_prev[1:-1] * 1e6)
+            self.create_lines(self.feature_prev[1:-1] )
         # Initialise slice and fit images
         self.plot_fit()
         self.plot_slice(self.slice_data, 'ccf')
@@ -1493,8 +1493,8 @@ class MainWindow(QtWidgets.QMainWindow, ephys_gui.Setup):
         if not self.histology_exists:
             return
 
-        if self.track[self.idx][-1] - 50 / 1e6 >= np.max(self.chn_depths) / 1e6:
-            self.track[self.idx] -= 50 / 1e6
+        if self.track[self.idx][-1] - 50  >= np.max(self.chn_depths) :
+            self.track[self.idx] -= 50 
             self.offset_button_pressed()
 
 
@@ -1506,8 +1506,8 @@ class MainWindow(QtWidgets.QMainWindow, ephys_gui.Setup):
         if not self.histology_exists:
             return
 
-        if self.track[self.idx][0] + 50 / 1e6 <= np.min(self.chn_depths) / 1e6:
-            self.track[self.idx] += 50 / 1e6
+        if self.track[self.idx][0] + 50  <= np.min(self.chn_depths) :
+            self.track[self.idx] += 50 
             self.offset_button_pressed()
 
     def toggle_labels_button_pressed(self):
@@ -1723,7 +1723,7 @@ class MainWindow(QtWidgets.QMainWindow, ephys_gui.Setup):
         self.plot_histology(self.fig_hist)
         self.plot_scale_factor()
         if np.any(self.feature_prev):
-            self.create_lines(self.feature_prev[1:-1] * 1e6)
+            self.create_lines(self.feature_prev[1:-1] )
         self.plot_fit()
         self.plot_channels()
         self.fig_hist.setYRange(min=self.probe_tip - self.probe_extra,
