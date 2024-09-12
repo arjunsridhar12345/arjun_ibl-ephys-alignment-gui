aaeb93224fbc1407174bd4bc4e77e5740f4c3c25
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2024-09-11 17:13:18 -0700

    increase height and width

--- src/ephys_alignment_gui/load_data_local.py
+++ src/ephys_alignment_gui/load_data_local.py
@@ -228,10 +228,10 @@ class LoadDataLocal:
         )
         #label_slice = np.swapaxes(label_slice, 0, 1)
 
-        width = [0, self.brain_atlas.image.shape[0]]
+        width = [0, 800]
         height = [
             0,
-            self.brain_atlas.image.shape[2],
+            500,
         ]
 
         slice_data = {
