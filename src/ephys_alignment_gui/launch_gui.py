d2775beabfacc5be921fe06f879c51b3bc2986b7
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2024-09-13 15:01:31 -0700

    debug hist data

--- src/ephys_alignment_gui/launch_gui.py
+++ src/ephys_alignment_gui/launch_gui.py
@@ -584,10 +584,11 @@ class MainWindow(QtWidgets.QMainWindow, ephys_gui.Setup):
         axis.setZValue(10)
         self.set_axis(self.fig_hist, 'bottom', pen='w', label='blank')
 
+        print(self.hist_data)
         # Plot each histology region
         for ir, reg in enumerate(self.hist_data['region']):
-            reg[0] = reg[0] * 20
-            reg[1] = reg[1] * 20
+            reg[0] = reg[0] * 10
+            reg[1] = reg[1] * 10
 
             colour = QtGui.QColor(*self.hist_data['colour'][ir])
             region = pg.LinearRegionItem(values=(reg[0], reg[1]),
