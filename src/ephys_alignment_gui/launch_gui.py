9e0730075005ccf6064c1f8e2508ebb4164bc2c0
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2024-09-13 10:43:10 -0700

    try to add offset to probe top

--- src/ephys_alignment_gui/launch_gui.py
+++ src/ephys_alignment_gui/launch_gui.py
@@ -587,7 +587,7 @@ class MainWindow(QtWidgets.QMainWindow, ephys_gui.Setup):
         # Plot each histology region
         for ir, reg in enumerate(self.hist_data['region']):
             colour = QtGui.QColor(*self.hist_data['colour'][ir])
-            region = pg.LinearRegionItem(values=(reg[0], reg[1] + 3840),
+            region = pg.LinearRegionItem(values=(reg[0] + self.probe_top, reg[1]),
                                          orientation=pg.LinearRegionItem.Horizontal,
                                          brush=colour, movable=False)
             # Add a white line at the boundary between regions
