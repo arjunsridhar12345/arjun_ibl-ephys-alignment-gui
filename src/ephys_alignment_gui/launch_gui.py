8fce6e406b4b8458cdbb122bda33a167c795706d
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2024-09-13 09:29:08 -0700

    check length of xyz channels

--- src/ephys_alignment_gui/launch_gui.py
+++ src/ephys_alignment_gui/launch_gui.py
@@ -920,7 +920,7 @@ class MainWindow(QtWidgets.QMainWindow, ephys_gui.Setup):
         self.channel_status = True
         self.xyz_channels = self.ephysalign.get_channel_locations(self.features[self.idx],
                                                                   self.track[self.idx])
-        print('Channels', self.xyz_channels)
+        print('Channels', len(self.xyz_channels))
         if not self.slice_chns:
             self.slice_lines = []
             self.slice_chns = pg.ScatterPlotItem()
