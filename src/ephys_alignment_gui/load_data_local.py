12c524dc9f4d982bec9d42a834c8d60a632e3296
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2024-09-14 16:25:16 -0700

    more scaling

--- src/ephys_alignment_gui/load_data_local.py
+++ src/ephys_alignment_gui/load_data_local.py
@@ -245,8 +245,8 @@ class LoadDataLocal:
 
         width = [0, self.brain_atlas.image.shape[0]]
         height = [
-            index[0, 2],
-            index[-1, 2],
+            index[0, 1],
+            index[-1, 1],
         ]
 
         print('Ccf slice', ccf_slice.shape)
