72934a60ebd515376bacbf6b648ce6112eed46b6
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2024-09-14 15:45:07 -0700

    correct height size of image slice

--- src/ephys_alignment_gui/load_data_local.py
+++ src/ephys_alignment_gui/load_data_local.py
@@ -248,7 +248,7 @@ class LoadDataLocal:
         print('Ccf slice shape', ccf_slice.shape)
         width = [0, self.brain_atlas.image.shape[0]]
         height = [
-            self.brain_atlas.image.shape[2],
+            self.brain_atlas.image.shape[1],
             0,
         ]
 
