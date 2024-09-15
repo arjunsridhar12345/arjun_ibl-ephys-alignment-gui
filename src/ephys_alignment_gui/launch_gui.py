0c91e2836064ea6d7f02bbbb2610037178afa398
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2024-09-14 20:07:15 -0700

    debug track coordinates

--- src/ephys_alignment_gui/launch_gui.py
+++ src/ephys_alignment_gui/launch_gui.py
@@ -909,7 +909,7 @@ class MainWindow(QtWidgets.QMainWindow, ephys_gui.Setup):
         self.traj_line = pg.PlotCurveItem()
 
         print('xyz track', self.xyz_track)
-        self.traj_line.setData(x=self.loaddata.brain_atlas.image.shape[0] - (self.xyz_track[1:-1, 0] * 1e6 / self.loaddata.brain_atlas.spacing), 
+        self.traj_line.setData(x=self.xyz_track[1:-1, 0] * 1e6 / self.loaddata.brain_atlas.spacing, 
                                y=self.xyz_track[1:-1, 2] * 1e6 / self.loaddata.brain_atlas.spacing, pen=self.kpen_solid)
         self.fig_slice.addItem(self.traj_line)
         self.plot_channels()
