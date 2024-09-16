53c534683f12cabbe35f73f790e9762345770f8e
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2024-09-16 14:01:17 -0700

    fix syntax error

--- src/ephys_alignment_gui/ephys_alignment.py
+++ src/ephys_alignment_gui/ephys_alignment.py
@@ -50,7 +50,7 @@ class EphysAlignment:
 
         self.region, self.region_label, self.region_colour, self.region_id\
             = self.get_histology_regions(self.xyz_samples, self.sampling_trk, self.brain_atlas)
-        print('label', type(self.region_label[0])
+        print('label', type(self.region_label[0]))
 
     def get_insertion_track(self, xyz_picks, speedy=False):
         """
