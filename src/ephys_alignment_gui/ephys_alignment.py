c1cd951e20f23e36c4ec22bd5f41bfc4511e1475
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2024-09-16 09:13:59 -0700

    change track length and tip distance

--- src/ephys_alignment_gui/ephys_alignment.py
+++ src/ephys_alignment_gui/ephys_alignment.py
@@ -89,8 +89,8 @@ class EphysAlignment:
         xyz_track = xyz_track[indices, :]
 
         # Compute distance to first electrode from bottom coordinate
-        tip_distance = _cumulative_distance(xyz_track)[1] + TIP_SIZE_UM / 1e6
-        track_length = _cumulative_distance(xyz_track)[-1]
+        tip_distance = _cumulative_distance(xyz_track)[-1] + TIP_SIZE_UM / 1e6
+        track_length = _cumulative_distance(xyz_track)[1]
         track_extent = np.array([0, track_length]) - tip_distance
         return xyz_track, track_extent
 
