5ec8fdb1e31df7369f59fe45dc796f8f30749448
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2024-09-23 10:04:57 -0700

    fix scaling

--- src/ephys_alignment_gui/ephys_alignment.py
+++ src/ephys_alignment_gui/ephys_alignment.py
@@ -34,7 +34,7 @@ class EphysAlignment:
             self.track_init = np.array([-1 * start_lims, start_lims])
             self.feature_init = np.array([-1 * start_lims, start_lims])
 
-        self.sampling_trk = np.arange(self.track_extent[-1] - 10 * 1e-6, self.track_extent[0] - 10 * 1e6, -10 * 1e-6)
+        self.sampling_trk = np.arange(self.track_extent[-1] - 10 * 1e-6, self.track_extent[0] - 10 * 1e-6, -10 * 1e-6)
         
         self.xyz_samples = histology.interpolate_along_track(self.xyz_track,
                                                              self.sampling_trk -
