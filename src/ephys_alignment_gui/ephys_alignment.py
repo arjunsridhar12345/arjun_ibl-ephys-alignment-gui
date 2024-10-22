acb4e812afdd2ea1b30d5ca11148840d88eae31b
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2024-10-22 16:18:25 -0700

    revert to original entry

--- src/ephys_alignment_gui/ephys_alignment.py
+++ src/ephys_alignment_gui/ephys_alignment.py
@@ -70,8 +70,7 @@ class EphysAlignment:
 
         # Force the entry to be on the upper z lim of the atlas to account for cases where channels
         # may be located above the surface of the brain
-        entry = atlas.Insertion.get_brain_entry(traj_entry, self.brain_atlas)
-        entry[2] = 0
+        entry = (traj_exit.eval_z(self.brain_atlas.bc.zlim))[0, :]
         
         if speedy:
             exit = (traj_exit.eval_z(self.brain_atlas.bc.zlim))[1, :]
