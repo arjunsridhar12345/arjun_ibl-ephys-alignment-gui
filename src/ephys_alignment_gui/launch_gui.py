cb8937a6c745a9eafbe3a6f87e256449b88d5b17
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2024-10-24 11:37:44 -0700

    fix function

--- src/ephys_alignment_gui/launch_gui.py
+++ src/ephys_alignment_gui/launch_gui.py
@@ -1171,7 +1171,7 @@ class MainWindow(QtWidgets.QMainWindow, ephys_gui.Setup):
         print('Feature prev', self.feature_prev)
 
     def switch_image_space(self) -> None:
-        if 'CCF' in self.switch_space_button.getText():
+        if 'CCF' in self.switch_space_button.text():
             print('hello')
 
     def load_existing_alignments(self):
