49b35388604ff6c8b27aac55b0a36c0e5c3371a5
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2024-10-30 11:31:20 -0700

    use correct path when searching for transforms

--- src/ephys_alignment_gui/launch_gui.py
+++ src/ephys_alignment_gui/launch_gui.py
@@ -1777,16 +1777,16 @@ class MainWindow(QtWidgets.QMainWindow, ephys_gui.Setup):
                                       'y': image_physical_space_coordinates[:, 1], 
                                       'z': image_physical_space_coordinates[:, 2]})
         
-        smartspim_template_affine_transform = tuple(self.input_path.glob('*/image_atlas_alignment/*/ls_to_template_SyN_0GenericAffine.mat'))
+        smartspim_template_affine_transform = tuple(DATA_PATH.glob('*/image_atlas_alignment/*/ls_to_template_SyN_0GenericAffine.mat'))
         if not smartspim_template_affine_transform:
             # try legacy way
-            smartspim_template_affine_transform = tuple(self.input_path.glob('*/registration/ls_to_template_SyN_0GenericAffine.mat'))
+            smartspim_template_affine_transform = tuple(DATA_PATH.glob('*/registration/ls_to_template_SyN_0GenericAffine.mat'))
             if not smartspim_template_affine_transform:
                 raise FileNotFoundError('No affine transform from spim to template. Check attached assets')
 
-        smartspim_template_warp_transform = tuple(self.input_path.glob('*/image_atlas_alignment/*/ls_to_template_SyN_1InverseWarp.nii.gz'))
+        smartspim_template_warp_transform = tuple(DATA_PATH.glob('*/image_atlas_alignment/*/ls_to_template_SyN_1InverseWarp.nii.gz'))
         if not smartspim_template_warp_transform:
-            smartspim_template_warp_transform = tuple(self.input_path.glob('*/registration/ls_to_template_SyN_1InverseWarp.nii.gz'))
+            smartspim_template_warp_transform = tuple(DATA_PATH.glob('*/registration/ls_to_template_SyN_1InverseWarp.nii.gz'))
             if not smartspim_template_warp_transform:
                 raise FileNotFoundError('No warp transform from spim to template. Check attached assets')
         
