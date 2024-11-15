5c241bfb1892551343b35d8362924d36c8bbecdb
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2024-11-15 10:43:55 -0800

    try to fix exit

--- src/ephys_alignment_gui/ephys_alignment.py
+++ src/ephys_alignment_gui/ephys_alignment.py
@@ -72,13 +72,15 @@ class EphysAlignment:
         # may be located above the surface of the brain
         entry = (traj_entry.eval_z(self.brain_atlas.bc.zlim))[0, :]
         
+        """
         if speedy:
             exit = (traj_exit.eval_z(self.brain_atlas.bc.zlim))[1, :]
         else:
             exit = atlas.Insertion.get_brain_exit(traj_exit, self.brain_atlas)
             # The exit is just below the bottom surfacce of the brain
             exit[2] = exit[2] - 200 / 1e6
-        
+        """
+        exit = (traj_exit.eval_z(self.brain_atlas.bc.zlim))[1, :]
         # Catch cases where the exit
         if any(np.isnan(exit)):
             exit = (traj_exit.eval_z(self.brain_atlas.bc.zlim))[1, :]
