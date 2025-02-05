494fdf9b7eba650cba02a396926efb6b024c0577
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2025-02-04 16:53:06 -0800

    move rounding

--- src/ephys_alignment_gui/ephys_alignment.py
+++ src/ephys_alignment_gui/ephys_alignment.py
@@ -498,9 +498,9 @@ class EphysAlignment:
             extent = 500e-6
             vector = np.diff(xyz, axis=0)[0]
             point = xyz[0, :]
-            vector_perp = np.round(np.array([1, 0, -1 * vector[0] / vector[2]]))
-            xyz_per = np.r_[[point + (-1 * extent * vector_perp)],
-                            [point + (extent * vector_perp)]]
+            vector_perp = np.array([1, 0, -1 * vector[0] / vector[2]])
+            xyz_per = np.round(np.r_[[point + (-1 * extent * vector_perp)],
+                            [point + (extent * vector_perp)]])
             slice_lines.append(xyz_per)
 
         return slice_lines
