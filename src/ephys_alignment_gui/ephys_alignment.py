c13e2a293bdda91b78dbdfffd8e65d16070ac21c
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2025-02-12 17:48:14 -0800

    take ceiling

--- src/ephys_alignment_gui/ephys_alignment.py
+++ src/ephys_alignment_gui/ephys_alignment.py
@@ -499,7 +499,7 @@ class EphysAlignment:
             vector = np.diff(xyz, axis=0)[0]
             point = xyz[0, :]
             vector_perp = np.array([1, 0, -1 * vector[0] / vector[2]])
-            xyz_per = np.round(np.r_[[point + (-1 * extent * vector_perp)],
+            xyz_per = np.ceil(np.r_[[point + (-1 * extent * vector_perp)],
                             [point + (extent * vector_perp)]])
             slice_lines.append(xyz_per)
 
