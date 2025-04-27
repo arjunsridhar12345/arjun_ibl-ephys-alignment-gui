c32eced1d1a702a7e4926cfa447e52bb4fbae551
arjunsridhar12345 <arjun.sridhar@alleninstitute.org>
2025-04-27 12:23:27 -0700

    revert volume flip

--- src/ephys_alignment_gui/custom_atlas.py
+++ src/ephys_alignment_gui/custom_atlas.py
@@ -269,8 +269,7 @@ class CustomAtlas(BrainAtlas):
         # Convert sitk to the (ap, ml, dv) np array needed by BrainAtlas
         #IMG2 = sitk.DICOMOrient(IMG,self.read_string) 
         self.original_image = IMG
-        #self.image = np.flip(sitk.GetArrayFromImage(IMG).T, axis=(0, 2))
-        self.image = sitk.GetArrayFromImage(IMG).T
+        self.image = np.flip(sitk.GetArrayFromImage(IMG).T, axis=(0, 2))
         print('Shape', self.image.shape)
         self.offset = IMG.GetOrigin()
         self.spacing = IMG.GetSpacing()[0] * 1000
@@ -280,5 +279,4 @@ class CustomAtlas(BrainAtlas):
         IMG = sitk.ReadImage(self.atlas_labels_file)
         # Convert sitk to the (ap, ml, dv) np array needed by BrainAtlas
         #IMG2 = sitk.DICOMOrient(IMG,self.read_string)
-        #self.label = np.flip(sitk.GetArrayFromImage(IMG).astype(np.int32).T, axis=(0, 2))
-        self.label = sitk.GetArrayFromImage(IMG).astype(np.int32).T
+        self.label = np.flip(sitk.GetArrayFromImage(IMG).astype(np.int32).T, axis=(0, 2))
