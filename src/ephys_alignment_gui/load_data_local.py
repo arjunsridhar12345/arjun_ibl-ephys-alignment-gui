61337df68fb2b80298a88cf2f057a57f322c4bbc
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2025-04-29 12:59:19 -0700

    try swapping to get LPS orientation

--- src/ephys_alignment_gui/load_data_local.py
+++ src/ephys_alignment_gui/load_data_local.py
@@ -244,6 +244,10 @@ class LoadDataLocal:
             user_picks = json.load(f)
 
         xyz_picks = np.array(user_picks["xyz_picks"]) / self.brain_atlas.spacing
+        temp = xyz_picks[:, 2]
+        xyz_picks[:, 0] = xyz_picks[:, 2]
+        xyz_picks[:, 2] = temp
+        
         xyz_picks[:, 0] = self.brain_atlas.image.shape[0] - xyz_picks[:, 0]
         xyz_picks[:, 2] = self.brain_atlas.image.shape[2] - xyz_picks[:, 2]
         xyz_picks = xyz_picks * self.brain_atlas.spacing / 1e6
